867f433179c2dd8db256649afe3cf2d7aabca7da
Germán <gertiscastilla@gmail.com>
2022-09-24 02:23:46 +0200

    nothing

--- Assets/Scripts/View/LoadScene.cs
+++ Assets/Scripts/View/LoadScene.cs
@@ -24,6 +24,7 @@ public class LoadScene : MonoBehaviour
 
     public void LoadThisScene(int sceneIndex)
     {
+        Debug.Log("Pulsado");
         StartCoroutine(LoadSceneAsync(sceneIndex));
     }
 
@@ -47,13 +48,15 @@ public class LoadScene : MonoBehaviour
         canvas.gameObject.SetActive(true);
         canvas.alpha = 0f;
 
+        Debug.Log("Antes de toda carga");
+
         while (canvas.alpha < 1f)
         {
             canvas.alpha += Time.deltaTime;
             yield return null;
         }
 
-        
+        Debug.Log("Despues alpha");
 
         foreach (Camera c in Camera.allCameras)
         { if (c != camera) { c.gameObject.SetActive(false); } }
@@ -63,17 +66,27 @@ public class LoadScene : MonoBehaviour
             AsyncOperation unloadOperation = SceneManager.UnloadSceneAsync(currentScene);
             while (!unloadOperation.isDone)
             { yield return null; }
+            Debug.Log("Despues Unload");
         }
         {
             AsyncOperation loadOperation = SceneManager.LoadSceneAsync(sceneIndex, LoadSceneMode.Additive);
             while (!loadOperation.isDone)
             { yield return null; }
+            Debug.Log("Despues carga");
+            while (SceneManager.GetActiveScene().buildIndex != sceneIndex)
+            {
+                yield return null;
+            }
+
+            Debug.Log("Dewspues carga 2");
         }
 
         while (SceneManager.sceneCount > 2)
         {
             yield return SceneManager.UnloadSceneAsync(SceneManager.GetSceneAt(2));
         }
+
+        Debug.Log("Eliminar scenes");
 
         camera.gameObject.SetActive(false);
         currentScene = SceneManager.GetSceneAt(1);
@@ -83,6 +96,8 @@ public class LoadScene : MonoBehaviour
             canvas.alpha -= Time.deltaTime;
             yield return null;
         }
+
+        Debug.Log("fin");
 
         canvas.gameObject.SetActive(false);
         StopAllCoroutines();
